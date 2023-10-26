89edd2acff6d72b91c64c8eb290823c59e3b42f5
marino barroso <marinobarroso2000@gmail.com>
2023-10-26 16:35:56 -0400

    Inicio de proyecto

---
+++ src/main/java/com/sis213/cardauction/CardAcuction/CardAcuctionApplication.java
@@ -0,0 +1,13 @@
+package com.sis213.cardauction.CardAcuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardAcuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardAcuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sis213/cardauction/CardAcuction/CardAcuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sis213.cardauction.CardAcuction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardAcuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
